d3389bf3eb9d931ac9bc21137bffba2bde4b4fa7
brettm12345 <brettmandler@gmail.com>
2020-02-07 06:59:22 -0500

    Update modules/applications/prompt.nix

--- modules/applications/prompt.nix
+++ modules/applications/prompt.nix
@@ -7,31 +7,31 @@ with import ../../support.nix { inherit lib config pkgs; }; {
       enableBashIntegration = true;
       enableFishIntegration = true;
       enableZshIntegration = true;
+      settings = with lib;
+        with builtins;
+        let
+          fromKey = key:
+            mapAttrs' (name: value: nameValuePair name { "${key}" = value; });
+          symbols = fromKey "symbol";
+          style = fromKey "style";
+          disable = x:
+            listToAttrs (map (name: nameValuePair name { disabled = true; }) x);
+        in (symbols {
+          aws = " ";
+          conda = " ";
+          git_branch = " ";
+          golang = " ";
+          package = " ";
+          rust = " ";
+        } // style { directory = "blue"; } // disable [ "battery" ] // {
+          character.style_success = "purple";
+          git_status = {
+            prefix = "";
+            suffix = " ";
+            style = "yellow";
+          };
+          nix_shell.use_name = true;
+        });
     };
-    xdg.configFile."starship.toml".text = with lib;
-      with builtins;
-      let
-        fromKey = key:
-          mapAttrs' (name: value: nameValuePair name { "${key}" = value; });
-        symbols = fromKey "symbol";
-        style = fromKey "style";
-        disable = x:
-          listToAttrs (map (name: nameValuePair name { disabled = true; }) x);
-      in genToml (symbols {
-        aws = " ";
-        conda = " ";
-        git_branch = " ";
-        golang = " ";
-        package = " ";
-        rust = " ";
-      } // style { directory = "blue"; } // disable [ "battery" ] // {
-        character.style_success = "purple";
-        git_status = {
-          prefix = "";
-          suffix = " ";
-          style = "yellow";
-        };
-        nix_shell.use_name = true;
-      });
   };
 }
