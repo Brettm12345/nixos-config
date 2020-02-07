cc43f098dbe563073895a5083df995457c102076
brettm12345 <brettmandler@gmail.com>
2020-02-07 07:30:33 -0500

    Update modules/applications/fish.nix

--- modules/applications/fish.nix
+++ modules/applications/fish.nix
@@ -10,14 +10,18 @@
       la = "${pkgs.exa}/bin/exa -F --icons -l --git -a -h -g";
       git = "${pkgs.gitAndTools.hub}/bin/hub";
     };
-    promptInit = ''
+    loginShellInit = ''
       set -U fish_greeting
-      ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
       set -x MANPAGER "${pkgs.neovim}/bin/nvim -c 'set ft=man' -"
       set -x BROWSER "${pkgs.chromium}/bin/chromium"
       set -x EDITOR "${pkgs.neovim}/bin/nvim"
       set -x NPM_DIR "$HOME/.npm/bin"
       set -U fish_user_paths "$HOME/bin" "$HOME/.cask/bin" "$HOME/.emacs.d/bin" "$NPM_DIR" "$CARGO_HOME/bin" "$HOME/.local/bin"
+    '';
+    promptInit = ''
+      ${pkgs.direnv}/bin/direnv hook fish | source
+      ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
+      ${pkgs.starship}/bin/starship fish | source
       source ${pkgs.imports.forgit}/forgit.plugin.fish
     '';
   };
