c9d1e31c27c846b0bb6757420a69e5ec52f54c61
brettm12345 <brettmandler@gmail.com>
2020-02-09 03:44:38 -0500

    Update modules/boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,13 @@
 { ... }: {
   boot = {
+    plymouth.enable = true;
     loader = {
       efi.canTouchEfiVariables = false;
       grub = {
         efiSupport = true;
         efiInstallAsRemovable = true;
         gfxmodeEfi = "1280x1024";
+        gfxpayloadEfi = "keep";
         device = "nodev";
       };
     };
