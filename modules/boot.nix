577f6b5bfbf0a7872c7b1fd902865971cbdd219f
brettm12345 <brettmandler@gmail.com>
2020-02-09 03:04:32 -0500

    Update modules/boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,10 +7,6 @@
         gfxmodeEfi = "1280x1024";
         device = "/dev/nvme0n1p4";
       };
-      systemd-boot = {
-        enable = true;
-        consoleMode = "auto";
-      };
     };
   };
 }
