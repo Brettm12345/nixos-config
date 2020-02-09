5caafaf7e4b8027b658c6ca4e697e8efdfb4b6a2
brettm12345 <brettmandler@gmail.com>
2020-02-09 03:05:50 -0500

    Update modules/boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,9 +1,9 @@
 { ... }: {
   boot = {
     loader = {
-      efi.canTouchEfiVariables = true;
       grub = {
         efiSupport = true;
+        efiInstallAsRemovable = true;
         gfxmodeEfi = "1280x1024";
         device = "/dev/nvme0n1p4";
       };
