8ebd8f8cb8648b2e121d7ba111d16ee731db1bc5
brettm12345 <brettmandler@gmail.com>
2020-02-09 09:48:48 -0500

    Update modules/applications/zsh.nix

--- modules/applications/zsh.nix
+++ modules/applications/zsh.nix
@@ -2,8 +2,17 @@
   environment.pathsToLink = [ "/share/zsh" ];
   home-manager.users.brett.programs.zsh = {
     enable = true;
+    autocd = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    shellAliases = { "rebuild" = "sudo nixos-rebuild switch"; };
+    plugins = [{
+      name = "fast-syntax-highlighting";
+      src = pkgs.fetchFromGitHub {
+        owner = "zdharma";
+        repo = "fast-syntax-highlighting";
+        rev = "94b6b5b8e58aeecd7587a973dbe110a352d7314d";
+        sha256 = "1lvq9qk0jz65swbghg4j08353z27v7nhd1r5i454y91s6w6n4b46";
+      };
+    }];
   };
 }
