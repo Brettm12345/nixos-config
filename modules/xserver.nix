d212315633e02ddafd061b5c23d1d0634ca4872f
brettm12345 <brettmandler@gmail.com>
2020-02-09 02:53:41 -0500

    Update modules/xserver.nix

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ sddm-theme-goodnight ];
+  hardware.nvidia.modesetting.enable = true;
   # https://twitter.com/ttuegel/status/997561239659270145
   environment.pathsToLink = [ "/share" ];
   services.xserver = {
