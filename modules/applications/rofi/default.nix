ba4fd513c320d72ed7bf966d8e2f34aa426d430a
brettm12345 <brettmandler@gmail.com>
2020-02-08 23:55:04 -0500

    Update modules/applications/rofi/default.nix

--- modules/applications/rofi/default.nix
+++ modules/applications/rofi/default.nix
@@ -15,10 +15,10 @@ let
     display-keys = "גּ ";
     display-window = " ";
     display-windowcd = " ";
-    display-run = "";
+    display-run = " ";
     display-ssh = " ";
-    display-drun = " ";
-    display-combi = " ";
+    display-drun = "  ";
+    display-combi = "  ";
     matching = "fuzzy";
     run-command = "fish -c '{cmd}'";
     run-list-command = "'fish -c functions'";
