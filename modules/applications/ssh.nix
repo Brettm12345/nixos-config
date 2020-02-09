80de5c4a8ca7532a47961644b830911448f5c5bd
brettm12345 <brettmandler@gmail.com>
2020-02-09 00:59:23 -0500

    Update modules/applications/ssh.nix

--- modules/applications/ssh.nix
+++ modules/applications/ssh.nix
@@ -6,7 +6,6 @@
     forwardX11 = true;
     ports = [ 22 13722 ];
   };
-
   home-manager.users.brett.programs.ssh =
     if (!isNull config.secrets.id_rsa) then {
       enable = true;
@@ -21,17 +20,6 @@
           compression = false;
           addressFamily = "inet";
         };
-        aws = {
-          hostname = "ec2-54-172-188-27.compute-1.amazonaws.com";
-          user = "ec2-user";
-          identityFile = "$HOME/.ssh/yelli.pem";
-        };
-
-        magento = {
-          hostname = "209.126.24.127";
-          user = "a6ff38c0_1";
-        };
-
       };
     } else
       { };
