0d6e54e690baa850f54f33e4ce897d08327725fd
brettm12345 <brettmandler@gmail.com>
2020-02-09 03:11:23 -0500

    Update modules/boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,7 +6,7 @@
         efiSupport = true;
         efiInstallAsRemovable = true;
         gfxmodeEfi = "1280x1024";
-        device = "/dev/nvme0n1";
+        device = "nodev";
       };
     };
   };
