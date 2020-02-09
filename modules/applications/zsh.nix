96338246164c959de6cdd165defc982d710855b1
brettm12345 <brettmandler@gmail.com>
2020-02-09 13:02:26 -0500

    Update modules/applications/zsh.nix

--- modules/applications/zsh.nix
+++ modules/applications/zsh.nix
@@ -19,7 +19,13 @@
       zinit snippet OMZ::plugins/gitfast/_git
 
       zinit light softmoth/zsh-vim-mode
+      KEYTIMEOUT=5
+      MODE_CURSOR_VICMD="block"
+      MODE_CURSOR_VIINS="blinking bar"
+      MODE_CURSOR_SEARCH="steady underline"
+
       zinit ice wait atinit"zpcompinit" lucid
+
       zinit light zdharma/fast-syntax-highlighting
 
       bindkey '^e' autosuggest-accept
