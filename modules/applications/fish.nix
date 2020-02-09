45cd9185db0a2f35c85e136549851c6e5a1128ed
brettm12345 <brettmandler@gmail.com>
2020-02-08 23:31:52 -0500

    Update modules/applications/fish.nix

--- modules/applications/fish.nix
+++ modules/applications/fish.nix
@@ -4,6 +4,7 @@
   programs.fish = {
     enable = true;
     shellAliases = {
+      o = "open-project";
       l = "${pkgs.exa}/bin/exa -F --icons --git-ignore -1";
       ls = "${pkgs.exa}/bin/exa -F --icons --git-ignore";
       ll = "${pkgs.exa}/bin/exa -F --icons -l --git -h --git-ignore";
@@ -16,6 +17,7 @@
       set -U fish_user_paths "$HOME/bin" "$HOME/.cask/bin" "$HOME/.emacs.d/bin" "$NPM_DIR" "$CARGO_HOME/bin" "$HOME/.local/bin"
     '';
     promptInit = ''
+      bind -M insert \co 'open-project'
       ${pkgs.any-nix-shell}/bin/any-nix-shell fish  | source
       $HOME/.cargo/bin/starship init fish | source
       source ${pkgs.imports.forgit}/forgit.plugin.fish
