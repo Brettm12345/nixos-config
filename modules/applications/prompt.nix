071d0a776f39a7c26149a743541196e0ac29174f
brettm12345 <brettmandler@gmail.com>
2020-02-14 03:33:18 -0500

    Update modules/applications/prompt.nix,modules/applications/zsh/config.zsh

--- modules/applications/prompt.nix
+++ modules/applications/prompt.nix
@@ -6,7 +6,6 @@ with import ../../support.nix { inherit lib config pkgs; }; {
       enable = true;
       enableBashIntegration = true;
       enableFishIntegration = false;
-      enableZshIntegration = true;
       disabled = [ "battery" ];
       symbols = {
         aws = " ";
--- modules/applications/zsh/config.zsh
+++ modules/applications/zsh/config.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
-source ./completions.zsh
+source ./completion.zsh
 
 zinit ice wait"0a" compile:'{src/*.zsh,src/strategies/*}' lucid
 zinit light zsh-users/zsh-autosuggestions
+
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
 
 zinit ice wait"1b" as:"program" lucid
 zinit light zimfw/archive
@@ -17,13 +20,13 @@ zinit light softmoth/zsh-vim-mode
 zinit ice wait atinit"zpcompinit" lucid
 zinit light zdharma/fast-syntax-highlighting
 
-ZSH_AUTOSUGGEST_USE_ASYNC=1
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#5b6395"
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#5b6395"
 
-KEYTIMEOUT=1
-MODE_CURSOR_VICMD="block"
-MODE_CURSOR_VIINS="blinking bar"
-MODE_CURSOR_SEARCH="steady underline"
+export KEYTIMEOUT=1
+export MODE_CURSOR_VICMD="block"
+export MODE_CURSOR_VIINS="blinking bar"
+export MODE_CURSOR_SEARCH="steady underline"
 
 bindkey '^e' autosuggest-accept
 
