6e2a278246f8be32361fd30adff0ca7d4545fefe
brettm12345 <brettmandler@gmail.com>
2020-02-14 04:21:23 -0500

    Update modules/applications/zsh/config.zsh

--- modules/applications/zsh/config.zsh
+++ modules/applications/zsh/config.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+function prepend-sudo() {
+  LBUFFER="sudo $LBUFFER"
+}
+zle -N prepend-sudo
+
+bindkey ^s prepend-sudo
+
 zinit ice wait"0a" compile:'{src/*.zsh,src/strategies/*}' lucid
 zinit light zsh-users/zsh-autosuggestions
 
@@ -15,8 +22,9 @@ zinit light hlissner/zsh-autopair
 zinit ice wait"1b" lucid
 zinit light softmoth/zsh-vim-mode
 
-zinit ice wait"1b" lucid
-zinit light djui/alias-tips
+zinit ice from'gh-r' as'program'
+zinit light sei40kr/fast-alias-tips-bin
+zinit light sei40kr/zsh-fast-alias-tips
 
 zinit ice wait atinit"zpcompinit" lucid
 zinit light zdharma/fast-syntax-highlighting
