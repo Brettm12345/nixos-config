1e48f44bf7c57ff256d61b42204efcdb52b66704
brettm12345 <brettmandler@gmail.com>
2020-02-14 14:26:07 -0500

    Update modules/applications/zsh/config.zsh

--- modules/applications/zsh/config.zsh
+++ modules/applications/zsh/config.zsh
@@ -45,7 +45,6 @@ zle -N edit-command-line
 bindkey -a "^V" edit-command-line
 bindkey -v '^e^e' edit-command-line
 
-
 bindkey jk vi-cmd-mode
 
 function rebuild {
@@ -54,7 +53,6 @@ function rebuild {
   ./install
   cd $dir
 }
-
 
 autoload -Uz edit-command-line
 zle -N edit-command-line
@@ -66,60 +64,57 @@ autoload -Uz url-quote-magic
 zle -N self-insert url-quote-magic
 
 zinit ice wait"0a" light-mode lucid for \
-  compile'{src/*.zsh,src/strategies/*}' atload'_zsh_autosuggest_start' \
-      zsh-users/zsh-autosuggestions
-
-zinit as"program" src"./zsh/gh/gh.plugin.zsh"
-zinit light brettm12345/gh
-
-zinit ice wait"1b" atload"zpcdreplay"
-zinit light BuonOmo/yarn-completion
-
-zinit ice wait"1b" lucid
-zinit light zsh-users/zsh-history-substring-search
-
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='underline'
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND=''
-zle -N history-substring-search-up
-zle -N history-substring-search-down
-bindkey '^[OA' history-substring-search-up
-bindkey '^[OB' history-substring-search-down
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+    sei40kr/zsh-fast-alias-tips \
+    compile'{src/*.zsh,src/strategies/*}' atload'_zsh_autosuggest_start' \
+        zsh-users/zsh-autosuggestions \
+    atload"zpcdreplay" \
+        BuonOmo/yarn-completion
 
 
-zinit ice wait"0b" as"program" lucid
-zinit light zimfw/archive
+zinit ice wait"0b" light-mode lucid for \
+    pick"autopair.zsh" nocompletions \
+        hlissner/zsh-autopair \
+    atload"KEYTIMEOUT=20" \
+        softmoth/zsh-vim-mode \
+    atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
+        zinit light zdharma/fast-syntax-highlighting \
+    atload"bind_substring_search" \
+        zsh-users/zsh-history-substring-search
 
-zinit ice wait"0b" lucid
-zinit light hlissner/zsh-autopair
+function bind_substring_search {
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='underline'
+    HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND=''
+    zle -N history-substring-search-up
+    zle -N history-substring-search-down
+    bindkey '^[OA' history-substring-search-up
+    bindkey '^[OB' history-substring-search-down
+    bindkey -M vicmd 'k' history-substring-search-up
+    bindkey -M vicmd 'j' history-substring-search-down
+}
 
-zinit ice wait"0b" atload"KEYTIMEOUT=20" lucid
-zinit light softmoth/zsh-vim-mode
 
-zinit ice wait"0b" lucid for wfxr/forgit
+zinit light-mode wait"0c" as"program" lucid for \
+    zimfw/archive \
+    make"!" atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" pick"direnv" src"zhook.zsh" \
+        direnv/direnv \
+    make"!" src"./_shell/_pmy.zsh" pick"$ZPFX/bin/pmy" \
+        relastle/pmy \
+    from'gh-r' \
+        zinit light sei40kr/fast-alias-tips-bin
 
-zinit ice wait"0b" from'gh-r' as'program'
-zinit light sei40kr/fast-alias-tips-bin
-zinit light sei40kr/zsh-fast-alias-tips
+zt light-mode for \
+    trigger-load'!gh' src"./zsh/gh/gh.plugin.zsh" \
+        brettm12345/gh
+    trigger-load'!x' \
+        OMZ::plugins/extract/extract.plugin.zsh \
+    trigger-load'!ga;!gcf;!gclean;!gd;!glo;!grh;!gss' \
+        wfxr/forgit \
+    trigger-load'!gencomp' pick'zsh-completion-generator.plugin.zsh' blockf \
+    atload'alias gencomp="zinit lucid nocd as\"null\" wait\"1\" atload\"zinit creinstall -q _local/config-files; zpcompinit\" for /dev/null; gencomp"' \
+        RobSis/zsh-completion-generator
 
 zinit ice depth=1 atload'!source ~/.p10k.zsh'
 zinit light romkatv/powerlevel10k
-
-zinit as"program" make"!" src"./_shell/_pmy.zsh" pick"$ZPFX/bin/pmy" for relastle/pmy
-
-zinit as"program" make"!" atclone"./direnv hook zsh > zhook.zsh" \
-  atpull"%atclone" pick"direnv" src"zhook.zsh" for direnv/direnv
-
-zinit ice wait"0b" atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" lucid
-zinit light zdharma/fast-syntax-highlighting
-
-zinit light-mode wait"0c" for \
-    zinit-zsh/z-a-test \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-submods \
-    zinit-zsh/z-a-bin-gem-node \
-    zinit-zsh/z-a-rust
 
 typeset -U PATH path
 path=("$HOME/bin" "$path[@]")
