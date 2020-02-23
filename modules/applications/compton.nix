8038a3846f77ae667f15f5264b27cbe4a892feaa
brettm12345 <brettmandler@gmail.com>
2020-02-23 18:33:57 -0500

    Update modules/applications/compton.nix

--- modules/applications/compton.nix
+++ modules/applications/compton.nix
@@ -2,10 +2,32 @@
   home-manager.users.brett = {
     services.compton = {
       enable = true;
+      extraOptions = ''
+        sw-opti = true;
+        xrender-sync-fence = true;
+        fade-delta = 4;  # 30;
+        fade-in-step = 0.03;
+        fade-out-step = 0.03;
+        # no-fading-openclose = true;
+        fade-exclude = [ ];
+        vsync = "opengl-swc";
+        use-ewmh-active-win = true;
+        shadow = true;
+        no-dnd-shadow = true;
+        no-dock-shadow = true;
+        clear-shadow = true;
+        shadow-radius = 7;
+        shadow-offset-x = -7;
+        shadow-offset-y = -7;
+        shadow-opacity = 0.5;
+        # shadow-red = 0.0;
+        # shadow-green = 0.0;
+        # shadow-blue = 0.0;
+            '';
       backend = "glx";
       refreshRate = 60;
       fade = true;
-      fadeDelta = 5;
+      fadeDelta = 4;
       shadow = true;
       shadowExclude = [
         "_GTK_FRAME_EXTENTS@:c"
