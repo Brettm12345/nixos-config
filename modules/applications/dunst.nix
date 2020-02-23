e4abc051c52b25bbab20a845a7e3d8d07d24bf3d
brettm12345 <brettmandler@gmail.com>
2020-02-23 17:32:29 -0500

    Update modules/applications/dunst.nix

--- modules/applications/dunst.nix
+++ modules/applications/dunst.nix
@@ -2,8 +2,8 @@
 let
   thm = config.themes;
   timeouts = {
-    low = 4;
-    normal = 6;
+    low = 8;
+    normal = 16;
     critical = 0;
   };
 in with thm.colors;
@@ -13,21 +13,35 @@ with pkgs;
 with builtins; {
   home-manager.users.brett = {
     services.dunst = {
-      enable = false;
+      enable = true;
       iconTheme = {
         name = "Papirus-Dark";
         package = papirus-icon-theme;
       };
       settings = {
         global = {
-          geometry = "1000x50-10+10";
+          # [{width}x{height}][+/-{x}+/-{y}]
+          geometry = "365x15-21+21";
           transparency = 0;
+          dmenu = "${pkgs.rofi}/bin/rofi -dmenu -p dunst";
           font = sansSerif;
           padding = 16;
+          history_length = 20;
           horizontal_padding = 16;
+          idle_threshold = 120;
+          ignore_newline = false;
+          indicate_hidden = true;
+          max_icon_size = 64;
+          icon_position = "right";
+          line_height = 0;
+          separator_color = thm.colors.grayscale.base1;
+          separator_height = 2;
+          show_age_threshold = 60;
           word_wrap = true;
           follow = "mouse";
-          format = "<b>%s</b> %b";
+          format = ''
+            <b>%s</b>
+            %b'';
           markup = "full";
         };
       } // mapAttrs' (name: timeout:
